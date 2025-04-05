a208d91cc6e5bc48415335266fd371ba80807b2d
Yedrolo <lyedro@alumno.huergo.edu.ar>
2025-04-05 11:06:52 -0300

    added migrations and data

---
+++ drizzle/0002_jittery_wallop.sql
@@ -0,0 +1,9 @@
+CREATE TABLE "posts" (
+	"id" integer PRIMARY KEY GENERATED ALWAYS AS IDENTITY (sequence name "posts_id_seq" INCREMENT BY 1 MINVALUE 1 MAXVALUE 2147483647 START WITH 1 CACHE 1),
+	"id_user" integer NOT NULL,
+	"title" varchar(255) NOT NULL,
+	"comment" varchar(255) NOT NULL,
+	CONSTRAINT "posts_comment_unique" UNIQUE("comment")
+);
+--> statement-breakpoint
+ALTER TABLE "posts" ADD CONSTRAINT "posts_id_user_users_id_fk" FOREIGN KEY ("id_user") REFERENCES "public"."users"("id") ON DELETE no action ON UPDATE no action;
