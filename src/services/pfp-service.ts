06f84a67ef4d697f1e95e358b00d1b9d081258d3
gazoil2 <benicio920179@gmail.com>
2025-10-21 20:15:43 -0300

    dasdas

--- src/services/pfp-service.ts
+++ src/services/pfp-service.ts
@@ -22,7 +22,7 @@ export const uploadToMinio = async (file: Express.Multer.File, userId: string) =
     );
 
     // Retornar la URL pública del archivo
-    const fileUrl = `http://localhost:9000/${bucketName}/${fileName}`;
+    const fileUrl = `http://${process.env.MINIO_URL}:${process.env.MINIO_PORT}/${bucketName}/${fileName}`;
     return fileUrl;
   } catch (error) {
     console.error("Error al subir la imagen a MinIO:", error);
